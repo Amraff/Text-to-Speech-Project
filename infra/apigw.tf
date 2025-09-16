648c4332ea71cb718a38b1202635e69558949f96
Amraff <raphewkarim@gmail.com>
2025-09-16 09:36:45 +0000

    Adding wesite files and all resources

---
+++ infra/apigw.tf
@@ -0,0 +1,90 @@
+resource "aws_api_gateway_rest_api" "api" {
+  name = "polly-api"
+}
+
+# /new_post resource + POST
+resource "aws_api_gateway_resource" "new_post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "new_post"
+}
+
+resource "aws_api_gateway_method" "new_post_post" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.new_post.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "new_post_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.new_post.id
+  http_method = aws_api_gateway_method.new_post_post.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.new_post.invoke_arn
+}
+
+# /get-post resource + GET
+resource "aws_api_gateway_resource" "get_post" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "get-post"
+}
+
+resource "aws_api_gateway_method" "get_post_get" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.get_post.id
+  http_method   = "GET"
+  authorization = "NONE"
+  request_parameters = {
+    "method.request.querystring.postId" = false
+  }
+}
+
+resource "aws_api_gateway_integration" "get_post_integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.get_post.id
+  http_method = aws_api_gateway_method.get_post_get.http_method
+
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.get_post.invoke_arn
+}
+
+# Allow API Gateway to invoke the lambdas
+resource "aws_lambda_permission" "apigw_invoke_new_post" {
+  statement_id  = "AllowAPIGatewayInvokeNewPost"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.new_post.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_lambda_permission" "apigw_invoke_get_post" {
+  statement_id  = "AllowAPIGatewayInvokeGetPost"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.get_post.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+# Deployment & stage
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_integration.new_post_integration,
+    aws_api_gateway_integration.get_post_integration
+  ]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+
+  triggers = {
+    redeploy = "${filebase64sha256("../deploy/new_post.zip")}-${filebase64sha256("../deploy/convert_to_audio.zip")}-${filebase64sha256("../deploy/get_post.zip")}"
+  }
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
